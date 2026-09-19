48ec8ab8d48df660a324b8d9e4d8954c9ca5cb58
godaytuwan01 <>
2026-09-19 23:20:22 +0000

    String manipulation

--- variables_and_types/string_manipulation/main.py
+++ variables_and_types/string_manipulation/main.py
@@ -1,1 +1,7 @@
 grocery_items = "milk cheese bread apples oranges chicken"
+words = grocery_items.split()
+
+dairy1 = words [0][0:4]
+dairy2 = words [1][0:6]
+bakery1 = words [2][0:5]
+print (f"{dairy1}, {dairy2}, {bakery1}")
